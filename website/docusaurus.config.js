421e9603b617683990362fdb098d1df4ca0f3598
XPDKasun <kasunx.kelaniya@gmail.com>
2023-05-06 03:22:39 +0530

    Updated to set correct basePath

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://xpd-kasun.github.io/',
+  url: 'https://xpd-kasun.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/tailwind-3dtransform-plugin/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
