0afda6cd633530b745e080b51cf13ad8cc812e8e
XPDKasun <kasunx.kelaniya@gmail.com>
2023-05-07 04:03:24 +0530

    Fixed 'edit' url in docusaurus config

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -46,7 +46,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/XPD-Kasun/tailwind-3dtransform-plugin/edit/website/',
+            'https://github.com/XPD-Kasun/tailwind-3dtransform-plugin/edit/main/website/',
         },
         blog: false,
         theme: {
